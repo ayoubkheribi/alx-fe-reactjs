e6caa04d89f47913995d3bd76ece6b033d67aadf
Ayoub <akhribi627@gmail.com>
2025-07-13 18:27:52 +0100

    finshed the assigment

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './UserProfile'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -36,6 +37,7 @@ function App() {
       <Header />;
       <MainContent />;
       <Footer />;
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
     </>
   )
 }
