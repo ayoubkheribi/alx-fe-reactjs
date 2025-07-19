2afaa9c20f16a68f2aacfe06a80e8063ef464319
Ayoub <akhribi627@gmail.com>
2025-07-19 17:52:29 +0100

    styling is done

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ const UserProfile = (props) => {
   return(
     <div style={{padding:'10px',margin: '10px',border: '1px solid gray'}}>
       <h2 style={{color:'blue'}}>{props.name}</h2>
-      <p style={{fontWeight:'bold'}}>Age: {props.age}</p>
+      <p><span style={{fontWeight:'bold'}}>Age: {props.age}</span></p>
       <p style={{border:'2px solid black'}}>Bio: {props.bio}</p>
     </div>
   );
