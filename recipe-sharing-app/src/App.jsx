dc08d587f4b6dfe6cfbe84a65c56f00c383f73bb
Ayoub <akhribi627@gmail.com>
2025-07-22 21:01:58 +0100

    task 1 is done

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+const App = () => {
+  return (
+    <div>
+      <RecipeList/>
+      <AddRecipeForm/>
+    </div>
+  )
+}
+
+export default App;
--- recipe-sharing-app/src/store.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand'
-
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipes: (newRecipe) => set(state => ({...state.recipes,newRecipe})),
-  setRecipes: (recipes) => set({recipes})
-}));
-
-export default useRecipeStore;
