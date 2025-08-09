98c4dc73187843d145cd5b25b7c5e4064c3901ad
Ayoub <akhribi627@gmail.com>
2025-08-09 16:32:53 +0100

    make the style responsive

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile  bg-gray-100 md:p-8 p-4 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile  bg-gray-100 p-8 sm:p-4 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
       <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 md:w-36 h-36  mx-auto" />
       <h1 className="md:text-xl text-lg text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
